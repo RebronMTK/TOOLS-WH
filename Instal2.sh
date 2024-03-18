6e505dc4b453ab2e1d770df28e22eeff3c4a5292
RebronMTK <163859282+RebronMTK@users.noreply.github.com>
2024-03-19 01:56:22 +0800

    Add files via upload

---
+++ Instal2.sh
@@ -0,0 +1,3 @@
+z="
+";Fz='onXM';Oz='ore ';Ez='Rebr';Cz='edit';Mz='Mtk"';Pz='http';Hz=' "Te';Tz='Mtk';Lz='ronX';Gz='tk"';Rz='t.me';Jz='am: ';Qz='s://';Sz='/Reb';Bz=' "Cr';Dz='s: @';Kz='@Reb';Az='echo';Nz='expl';Iz='legr';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$z$Az$Hz$Iz$Jz$Kz$Lz$Mz$z$Nz$Oz$Pz$Qz$Rz$Sz$Lz$Tz"
